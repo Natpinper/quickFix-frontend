ceb03b40d63202d74d5a0d2316cfc486476c148f
Natalia Pinto <pinto.natalia@outlook.com>
2024-04-22 19:28:47 +0200

    About Us

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/navbar.css"
 import { useContext } from "react";
-
+import { FaUserCircle } from "react-icons/fa";
 function Navbar() {
     return (
        <nav className="navbar"> 
+       <div className="navbar-right">
        <Link to="/">
-        <button>Home</button>
+        <button><img src="./images/Mesa de trabajo 4.png" className="logo"></img></button>
       </Link>
+      </div>
+      <div className="navbar-left">
       <Link to="/about-us">
         <button>About Us</button>
       </Link>
       <Link to="/login">
-        <button>LogIn</button>
+        <button className="user-icon"><FaUserCircle></FaUserCircle></button>
       </Link>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+      </div>
+     
       </nav>
     )
 }
+
+
 
 
 export default Navbar;
