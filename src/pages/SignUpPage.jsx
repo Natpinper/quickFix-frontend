a9975966b91439833f12932a4cac51a6c822957c
Natalia Pinto <pinto.natalia@outlook.com>
2024-05-04 13:16:28 +0200

    dropdown location for all location´s select options

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -5,7 +5,70 @@ import axios from "axios";
 import authService from "../services/auth.service";
 import "../styles/SignupPage.css";
 import "../styles/LoginPage.css";
-import userService from "../services/User.service";
+
+const locationArray = [
+  { city: "Almería", region: "Andalusia" },
+  { city: "Cádiz", region: "Andalusia" },
+  { city: "Córdoba", region: "Andalusia" },
+  { city: "Granada", region: "Andalusia" },
+  { city: "Huelva", region: "Andalusia" },
+  { city: "Jaén", region: "Andalusia" },
+  { city: "Málaga", region: "Andalusia" },
+  { city: "Sevilla", region: "Andalusia" },
+  { city: "Huesca", region: "Aragon" },
+  { city: "Teruel", region: "Aragon" },
+  { city: "Zaragoza", region: "Aragon" },
+  { city: "Oviedo", region: "Asturias" },
+  { city: "Formentera", region: "Balearic Islands" },
+  { city: "Ibiza", region: "Balearic Islands" },
+  { city: "Majorca", region: "Balearic Islands" },
+  { city: "Minorca", region: "Balearic Islands" },
+  { city: "Alava", region: "Basque Country" },
+  { city: "Biscay", region: "Basque Country" },
+  { city: "Gipuzkoa", region: "Basque Country" },
+  { city: "Las Palmas", region: "Canary Islands" },
+  { city: "Fuerteventura", region: "Canary Islands" },
+  { city: "Gran Canaria", region: "Canary Islands" },
+  { city: "Lanzarote", region: "Canary Islands" },
+  { city: "Santa Cruz de Tenerife", region: "Canary Islands" },
+  { city: "La Gomera", region: "Canary Islands" },
+  { city: "La Palma", region: "Canary Islands" },
+  { city: "Tenerife", region: "Canary Islands" },
+  { city: "Santander", region: "Cantabria" },
+  { city: "Avila", region: "Castile and León" },
+  { city: "Burgos", region: "Castile and León" },
+  { city: "León", region: "Castile and León" },
+  { city: "Palencia", region: "Castile and León" },
+  { city: "Salamanca", region: "Castile and León" },
+  { city: "Segovia", region: "Castile and León" },
+  { city: "Soria", region: "Castile and León" },
+  { city: "Valladolid", region: "Castile and León" },
+  { city: "Zamora", region: "Castile and León" },
+  { city: "Albacete", region: "Castilla-La Mancha" },
+  { city: "Ciudad Real", region: "Castilla-La Mancha" },
+  { city: "Cuenca", region: "Castilla-La Mancha" },
+  { city: "Guadalajara", region: "Castilla-La Mancha" },
+  { city: "Toledo", region: "Castilla-La Mancha" },
+  { city: "Barcelona", region: "Catalonia" },
+  { city: "Girona", region: "Catalonia" },
+  { city: "Lleida", region: "Catalonia" },
+  { city: "Tarragona", region: "Catalonia" },
+  { city: "Badajoz", region: "Extremadura" },
+  { city: "Cáceres", region: "Extremadura" },
+  { city: "A Coruña", region: "Galicia" },
+  { city: "Lugo", region: "Galicia" },
+  { city: "Ourense", region: "Galicia" },
+  { city: "Pontevedra", region: "Galicia" },
+  { city: "Logroño", region: "La Rioja" },
+  { city: "Madrid", region: "Community of Madrid" },
+  { city: "Murcia", region: "Region of Murcia" },
+  { city: "Pamplona", region: "Navarre" },
+  { city: "Alicante", region: "Valencian Community" },
+  { city: "Castellón", region: "Valencian Community" },
+  { city: "Valencia", region: "Valencian Community" },
+  { city: "Ceuta", region: "Ceuta" },
+  { city: "Melilla", region: "Melilla" },
+];
 
 
 
@@ -13,10 +76,13 @@ function SignUpPage(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [input, setInput] = useState("");
   const [location, setLocation] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const navigate = useNavigate();
+
+  const cityArray = locationArray.map((location) => location.city).sort();
 
   const handleEmail = (e) => setEmail(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
@@ -38,6 +104,10 @@ function SignUpPage(props) {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
       });
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    setLocation(e.target.value);
   };
 
   return (
@@ -74,13 +144,19 @@ function SignUpPage(props) {
         />
 
         <label>Location:</label>
-        <input
-          required={true}
-          type="text"
+        <select
           name="location"
+          className="location-select"
           value={location}
-          onChange={handleLocation}
-        />
+          onChange={handleChange}
+        >
+          <option value="">Select a location</option>
+          {cityArray.map((city, index) => (
+            <option key={index} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
       
       <button className="signup-button" type="submit">
         Sign Up
