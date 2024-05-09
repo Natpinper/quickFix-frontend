6987a5ac559c16505e7bcc2098ed88f050480d66
Natalia Pinto <pinto.natalia@outlook.com>
2024-05-09 19:26:43 +0200

    new navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,73 @@ import CreatePost from "./pages/CreatePost";
 import EditUser from "./pages/EditUser";
 import UserPostsPage from "./pages/UserPostsPage";
 import EditPost from "./pages/EditPost";
+import NewNavBar from "./components/newNavBar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NewNavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/post/:postId" element={<PostPage />} />
-        <Route path="/user/:userId" element={<UserDetailsPage/>} />
-        <Route path="/signup" element={<IsAnon><SignUpPage /></IsAnon>}/>
-        <Route path="/login" element={<IsAnon><LogIn/></IsAnon>} />
-        <Route path="/user/:userId/profile" element={<IsPrivate><MyProfile/></IsPrivate>}/>
-        <Route path="/user/:userId/post/create" element={<IsPrivate><CreatePost/></IsPrivate>}/>
-        <Route path="/user/:userId/profile/edit" element={<IsPrivate><EditUser/></IsPrivate>} />
-        <Route path= "/user/:userId/post/:postId" element= {<IsPrivate><UserPostsPage/></IsPrivate>}/>
-        <Route path="/user/:userId/post/:postId/edit" element={<IsPrivate><EditPost/></IsPrivate>} />
+        <Route path="/user/:userId" element={<UserDetailsPage />} />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignUpPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LogIn />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/user/:userId/profile"
+          element={
+            <IsPrivate>
+              <MyProfile />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/user/:userId/post/create"
+          element={
+            <IsPrivate>
+              <CreatePost />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/user/:userId/profile/edit"
+          element={
+            <IsPrivate>
+              <EditUser />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/user/:userId/post/:postId"
+          element={
+            <IsPrivate>
+              <UserPostsPage />
+            </IsPrivate>
+          }
+        />
+        <Route
+          path="/user/:userId/post/:postId/edit"
+          element={
+            <IsPrivate>
+              <EditPost />
+            </IsPrivate>
+          }
+        />
       </Routes>
       <Footer />
     </div>
