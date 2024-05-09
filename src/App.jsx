88dc68b6a7168455eff6ed382c07b889d3e12652
Natalia Pinto <pinto.natalia@outlook.com>
2024-05-09 21:16:36 +0200

    homepage style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/navbar";
 import HomePage from "./pages/HomePage";
 import AboutUs from "./pages/AboutUs";
 import LogIn from "./pages/LogIn";
